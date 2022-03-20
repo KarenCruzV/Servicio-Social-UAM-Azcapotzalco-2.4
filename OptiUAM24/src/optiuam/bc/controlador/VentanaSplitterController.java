10500c76d1325062dc6522b27cd1db7d281683d5
Arturo Borja <al2182004764@azc.uam.mx>
2022-03-19 20:36:23 -0600

    Cambios minimos

--- OptiUAM24/src/optiuam/bc/controlador/VentanaSplitterController.java
+++ OptiUAM24/src/optiuam/bc/controlador/VentanaSplitterController.java
@@ -7,7 +7,10 @@ package optiuam.bc.controlador;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,6 +22,12 @@ public class VentanaSplitterController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    public void cerrarVentana(ActionEvent event){
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
