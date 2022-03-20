b4614e6b4268bca0cf3df8910c3f2c69b2b07083
Arturo Borja <al2182004764@azc.uam.mx>
2022-03-19 20:35:36 -0600

    Cambios minimos

--- OptiUAM24/src/optiuam/bc/controlador/ConectorController.java
+++ OptiUAM24/src/optiuam/bc/controlador/ConectorController.java
@@ -10,8 +10,11 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 
 /**
@@ -52,9 +55,14 @@ public class ConectorController implements Initializable {
         return modo;
     }
     
-       
+    public void cerrarVentana(ActionEvent event){
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        Image icoDesconectar = new Image("/images/acercaDe.png"); 
         
         
     }    
